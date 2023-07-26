14e703133d3086a8f70260f0a9eb6451962869ef
Raj <rawatraj995@gmail.com>
2023-07-26 16:11:10 +0530

    setup

---
+++ backend/schemas/comment.js
@@ -0,0 +1,18 @@
+export default {
+
+    name: 'comment',
+    title : 'Comment',
+    type:'document',
+    fields :[
+        {
+            name:'postedBy',
+            title: 'postedBy',
+            type:'postedBy',
+        },
+        {
+            name:'comments',
+            title: 'Comments',
+            type:'string',
+        }
+    ]
+}
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin, googleLogout} from '@react-oauth/google'
+import { useNavigate } from "react-router-dom";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+import jwt_decode from 'jwt-decode';
+import { client } from '../client';
+import "../index.css";
+
+const Login = () => {
+
+  const navigate= useNavigate();
+  
+  const responseGoogle = (response) => {
+    
+    const decoded = jwt_decode(response.credential);
+    
+    localStorage.setItem('user',JSON.stringify(decoded));
+    const { name ,picture , sub} = decoded;
+
+    const doc = {
+        _id:sub,
+        _type:'user',
+        userName: name,
+        image: picture,
+    }
+    client.createIfNotExists(doc)
+          .then(()=>{
+            navigate('/',{replace:true})
+          })
+    
+      }
+
+  const user=false;
+  
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}>
+      <div className="flex h-screen w-screen ">
+        <div className="flex w-screen">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loopflex
+            justify-start
+            items-center
+            flex-col
+            h-screen
+            controls={false}
+            muted
+            autoPlay
+            className="w-full object-cover "
+          />
+        </div>
+        <div className="absolute flex flex-col justify-center items-center backdrop-brightness-50 w-full h-full">
+          <div className="">
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+          <div className="shadow-2xl mt-3 flex rounded-lg bg-white">
+          {user ? (
+            <div> Logged In </div>
+          ): (
+            <GoogleLogin
+              onSuccess={(response)=>responseGoogle(response)}
+              onError={() => console.log('Error')}
+            />
+          )}
+          
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default Login;
