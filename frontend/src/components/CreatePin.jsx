9e6b15359c82a180551880ecf324b6ea3e0f5c82
Raj <rawatraj995@gmail.com>
2023-08-12 22:37:50 +0530

    feat:post pinned

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React , {useState} from 'react'
+import {AiOutlineCloudUpload} from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+
+import { client } from '../client';
+import Spinner from './Spinner';
+
+//categories [{}]
+
 
 function CreatePin() {
   return (
